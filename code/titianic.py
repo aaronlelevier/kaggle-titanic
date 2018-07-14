5f39a950776e5e625f1be33efb64bdce3d1d16ef
aaronlelevier <aaron.lelevier@gmail.com>
2018-07-14 11:04:18 -0700

    Add:
    - calc total runtime
    - group hyper params and constants separately
    - add EPOCHS as a hyper param
    - add FamilySize as a feature

--- code/titianic.py
+++ code/titianic.py
@@ -1,4 +1,5 @@
 import argparse
+import time
 
 import numpy as np
 import pandas as pd
@@ -13,24 +14,27 @@ parser.set_defaults(predict=True)
 args = parser.parse_args()
 
 
-# Global VARs
-PATH = '../input/'
-
+# Hyper parameters
 LR = 0.1
 
+EPOCHS = 1
+
 BATCH_SIZE = 50
+
+HIDDEN_UNITS = [10]
+
+L1_REGULARIZATION_STRENGTH = 0.001
+
+KFOLD_SPLIT_SIZE = 5
+
+# Constants
+PATH = '../input/'
 
 N_SAMPLES = len(list(open(f'{PATH}train.csv'))) - 1 # skip header
 
 STEPS = np.ceil(N_SAMPLES // BATCH_SIZE)
 
-HIDDEN_UNITS = [5]
-
 N_CLASSES = 2
-
-L1_REGULARIZATION_STRENGTH = 0.001
-
-KFOLD_SPLIT_SIZE = 5
 
 
 def train_input_fn(features, labels):
@@ -74,46 +78,68 @@ def cross_validate(classifier, train_x_all, train_y_all):
         train_x_all (pd.DataFrame)
         train_y_all (np.ndarray)
     """
-    print(
-        f'All shapes - train_x_all: {len(train_x_all)}, train_y_all: {len(train_y_all)}'
-    )
     results = []
     kf = KFold(n_splits=KFOLD_SPLIT_SIZE)
-    for train_idx, val_idx in kf.split(train_x_all, train_y_all):
-        train_x = train_x_all.iloc[train_idx]
-        train_y = train_y_all[train_idx]
 
-        val_x = train_x_all.iloc[val_idx]
-        val_y = train_y_all[val_idx]
+    for epoch in range(EPOCHS):
+        print(f'epoch: {epoch}')
 
-        classifier.train(
-            input_fn=lambda: train_input_fn(train_x, train_y),
-            steps=STEPS)
+        for train_idx, val_idx in kf.split(train_x_all, train_y_all):
+            train_x = train_x_all.iloc[train_idx]
+            train_y = train_y_all[train_idx]
 
-        eval_result = classifier.evaluate(
-            input_fn=lambda: eval_input_fn(val_x, val_y))
-        results.append(eval_result)
+            val_x = train_x_all.iloc[val_idx]
+            val_y = train_y_all[val_idx]
+
+            classifier.train(
+                input_fn=lambda: train_input_fn(train_x, train_y),
+                steps=STEPS)
+
+            eval_result = classifier.evaluate(
+                input_fn=lambda: eval_input_fn(val_x, val_y))
+            results.append(eval_result)
 
     return results
 
 
-def get_feature_columns():
+def get_unique_family_sizes(train, test):
+    train['FamilySize'] = train['SibSp'] + train['Parch'] + 1
+    test['FamilySize'] = test['SibSp'] + test['Parch'] + 1
+    return sorted(list(set(list(test['FamilySize'].values) + list(train['FamilySize'].values))))
+
+
+def get_feature_columns(train, test):
+    """
+    Returns a list of tf feature_columns's
+
+    Args:
+        train (pd.DataFrame)
+        test (pd.DataFrame)
+    """
     numeric_column_age = tf.feature_column.numeric_column(key='Age')
+
     categorical_column_has_cabin = tf.feature_column.indicator_column(
         tf.feature_column.categorical_column_with_vocabulary_list(
             key='HasCabin', vocabulary_list=(0, 1)))
 
+    categorical_column_family_size = tf.feature_column.embedding_column(
+        tf.feature_column.categorical_column_with_vocabulary_list(
+            key='FamilySize', vocabulary_list=get_unique_family_sizes(train, test)),
+        dimension=2)
+
     return [
         numeric_column_age,
-        categorical_column_has_cabin
+        categorical_column_has_cabin,
+        categorical_column_family_size
     ]
 
 
 def get_features(df):
-    "Returns a feature engineeered DataFame"
+    "Returns a feature engineeered DataFrame"
     return {
         'Age': df['Age'].fillna(0),
-        'HasCabin': df["Cabin"].apply(lambda x: 0 if type(x) == float else 1)
+        'HasCabin': df["Cabin"].apply(lambda x: 0 if type(x) == float else 1),
+        'FamilySize' : df['SibSp'] + df['Parch'] + 1
     }
 
 
@@ -129,6 +155,8 @@ def make_predictions(classifier, test):
 
 
 def main():
+    start_time = time.time()
+
     train = pd.read_csv(f'{PATH}train.csv')
     print('train shape:', train.shape)
     train.head()
@@ -138,7 +166,7 @@ def main():
     test.tail()
 
     classifier = tf.estimator.DNNClassifier(
-        feature_columns=get_feature_columns(),
+        feature_columns=get_feature_columns(train, test),
         # Two hidden layers of 10 nodes each.
         hidden_units=HIDDEN_UNITS,
         # The model must choose between 3 classes.
@@ -153,13 +181,15 @@ def main():
     train_y_all = train['Survived']
     results = cross_validate(classifier, train_x_all, train_y_all)
     for r in results:
-        print('\nTest set accuracy: {accuracy:0.3f}\n'.format(**r))
+        print('\nTest set accuracy: {accuracy:0.4f}'.format(**r))
 
     if args.predict:
         print('make_predictions')
         make_predictions(classifier, test)
     else:
         print('NOT make_predictions')
+
+    print("time: {seconds} seconds".format(seconds=(time.time() - start_time)))
 
 
 if __name__ == '__main__':
