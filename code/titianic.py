f2592f0e3e25ce28c936d561d2672d05727b6ef0
aaronlelevier <aaron.lelevier@gmail.com>
2018-07-14 08:22:11 -0700

    reorganized into 'main() func' with all globals and non-global variables properly cased

--- code/titianic.py
+++ code/titianic.py
@@ -1,87 +1,51 @@
-from __future__ import print_function
-
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import tensorflow as tf
+import tensorflow as tf # pylint: disable=import-error
 from sklearn.model_selection import KFold
-from sklearn.preprocessing import LabelEncoder, StandardScaler
-from tqdm import tqdm
+
 
 PATH = '../input/'
 
-train = pd.read_csv(f'{PATH}train.csv')
-print('train shape:', train.shape)
-train.head()
+LR = 0.1
 
-test = pd.read_csv(f'{PATH}test.csv')
-print('test shape:', test.shape)
-test.tail()
+BATCH_SIZE = 50
 
-# Parameters
-learning_rate = 0.01
-training_epochs = 500 # 1000 - orig value
-display_step = 50
+N_SAMPLES = len(list(open(f'{PATH}train.csv'))) - 1 # skip header
 
-batch_size = 50
-train_steps = batch_size * 10
+STEPS = np.ceil(N_SAMPLES // BATCH_SIZE)
 
-n_samples = train.shape[0]
+HIDDEN_UNITS = [10, 10]
 
-train_split = .8
-index_split = int(n_samples*train_split)
-train_slice = slice(0, index_split)
-test_slice = slice(index_split, n_samples)
+N_CLASSES = 2
 
-train[train_slice].shape[0] + train[test_slice].shape[0] == train.shape[0] 
+L1_REGULARIZATION_STRENGTH = 0.001
 
-# Age as the only feature
-print('train Age is NaN:', len(train[train['Age'].isnull()]))
-print('test Age is NaN:', len(test[test['Age'].isnull()]))
+KFOLD_SPLIT_SIZE = 5
 
 
-# separated train and dev sets
-train_set = train[train_slice]
-dev_set = train[test_slice]
+def get_feature_columns():
+    numeric_column_age = tf.feature_column.numeric_column(key='Age')
 
-# Train / Dev set splits
-# train
-train_Y = train_set['Survived']
-train_X = train_set['Age'].fillna(0)
-
-train_x = {'Age': train_X}
-train_y = train_Y
-
-# test
-dev_Y = dev_set['Survived']
-dev_X = dev_set['Age'].fillna(0)
-
-dev_x = {'Age': dev_X}
-dev_y = dev_Y
+    return [
+        numeric_column_age
+    ]
 
 
-# Feature Columns
-numeric_column_age = tf.feature_column.numeric_column(key='Age')
-my_feature_columns = [
-    numeric_column_age
-]
-
-
-def train_input_fn(features, labels, batch_size):
+def train_input_fn(features, labels):
     """An input function for training"""
     # Convert the inputs to a Dataset.
     dataset = tf.data.Dataset.from_tensor_slices((dict(features), labels))
 
     # Shuffle, repeat, and batch the examples.
-    dataset = dataset.shuffle(1000).repeat().batch(batch_size)
+    dataset = dataset.shuffle(1000).repeat().batch(BATCH_SIZE)
 
     # Return the read end of the pipeline.
     return dataset.make_one_shot_iterator().get_next()
 
 
-def eval_input_fn(features, labels, batch_size):
+def eval_input_fn(features, labels):
     """An input function for evaluation or prediction"""
-    features=dict(features)
+    features = dict(features)
     if labels is None:
         # No labels, use only features.
         inputs = features
@@ -92,58 +56,70 @@ def eval_input_fn(features, labels, batch_size):
     dataset = tf.data.Dataset.from_tensor_slices(inputs)
 
     # Batch the examples
-    assert batch_size is not None, "batch_size must not be None"
-    dataset = dataset.batch(batch_size)
+    assert BATCH_SIZE is not None, "BATCH_SIZE must not be None"
+    dataset = dataset.batch(BATCH_SIZE)
 
     # Return the read end of the pipeline.
     return dataset.make_one_shot_iterator().get_next()
 
 
-classifier = tf.estimator.DNNClassifier(
-    feature_columns=my_feature_columns,
-    # Two hidden layers of 10 nodes each.
-    hidden_units=[10, 10],
-    # The model must choose between 3 classes.
-    n_classes=2,
-    optimizer=tf.train.ProximalAdagradOptimizer(
-      learning_rate=0.1,
-      l1_regularization_strength=0.001
-    ))
-
-
-def run_train(train_x, train_y):
+def run_train(classifier, train_x, train_y):
     train_samples = len(train_x['Age'])
     print(
         f'All shapes - train_x: {train_samples}, train_y: {len(train_y)}'
     )
     classifier.train(
-        input_fn=lambda: train_input_fn(train_x, train_y, 50),
-        steps=10)
+        input_fn=lambda: train_input_fn(train_x, train_y),
+        steps=STEPS)
 
 
-def cross_validate(train_x_all, train_y_all, split_size=5):
+def cross_validate(classifier, train_x_all, train_y_all):
     print(
         f'All shapes - train_x_all: {len(train_x_all)}, train_y_all: {len(train_y_all)}'
     )
     results = []
-    kf = KFold(n_splits=split_size)
+    kf = KFold(n_splits=KFOLD_SPLIT_SIZE)
     for train_idx, val_idx in kf.split(train_x_all, train_y_all):
         train_x = {'Age': train_x_all[train_idx]}
         train_y = train_y_all[train_idx]
         val_x = {'Age': train_x_all[val_idx]}
         val_y = train_y_all[val_idx]
 
-        run_train(train_x, train_y)
+        run_train(classifier, train_x, train_y)
 
         eval_result = classifier.evaluate(
-            input_fn=lambda: eval_input_fn(val_x, val_y, batch_size))
+            input_fn=lambda: eval_input_fn(val_x, val_y))
         results.append(eval_result)
 
     return results
 
-# main
-train_x_all = train['Age'].fillna(0)
-train_y_all = train['Survived']
-results = cross_validate(train_x_all, train_y_all)
-for r in results:
-    print('\nTest set accuracy: {accuracy:0.3f}\n'.format(**r))
+
+def main():
+    train = pd.read_csv(f'{PATH}train.csv')
+    print('train shape:', train.shape)
+    train.head()
+
+    test = pd.read_csv(f'{PATH}test.csv')
+    print('test shape:', test.shape)
+    test.tail()
+
+    classifier = tf.estimator.DNNClassifier(
+        feature_columns=get_feature_columns(),
+        # Two hidden layers of 10 nodes each.
+        hidden_units=HIDDEN_UNITS,
+        # The model must choose between 3 classes.
+        n_classes=N_CLASSES,
+        optimizer=tf.train.ProximalAdagradOptimizer(
+            learning_rate=LR,
+            l1_regularization_strength=L1_REGULARIZATION_STRENGTH
+        ))
+
+    train_x_all = train['Age'].fillna(0)
+    train_y_all = train['Survived']
+    results = cross_validate(classifier, train_x_all, train_y_all)
+    for r in results:
+        print('\nTest set accuracy: {accuracy:0.3f}\n'.format(**r))
+
+
+if __name__ == '__main__':
+    main()
